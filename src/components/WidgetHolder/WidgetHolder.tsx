72f1e2441c9aafaa19d96939eca9ab956bd1af50
Yash Chaudhari <yashchaudhari008@gmail.com>
2024-01-22 19:07:53 +0530

    basic widget component added

--- src/components/WidgetHolder/WidgetHolder.tsx
+++ src/components/WidgetHolder/WidgetHolder.tsx
@@ -1,8 +1,30 @@
+import { getWidgets, WidgetType } from "../../utils/widgetsUtils";
+import type { WidgetData } from "../../utils/widgetsUtils";
+
+import AddNewWidgetBtn from "../widgets/AddNewWidgetBtn/AddNewWidgetBtn"
+import BookmarkWidget from "../widgets/BookmarkWidget/BookmarkWidget"
 import styles from "./widgetHolder.module.scss"
 
 const WidgetHolder = () => {
+  const widgetsData = getWidgets();
+
+  const getWidgetDOM = (widgetData: WidgetData, index: number) => {
+    switch (widgetData.type) {
+      case WidgetType.BookmarkWidget:
+        return <BookmarkWidget key={index} {...widgetData} />
+    }
+  }
   return (
-    <div className={styles.widgetHolder}>WidgetHolder</div>
+    <div className={styles.widgetHolderWrapper}>
+      <div className={styles.widgetHolder}>
+        {widgetsData.map((widgetData, index) => {
+          return <div className={styles.widget}>
+            {getWidgetDOM(widgetData, index)}
+          </div>
+        })}
+        <AddNewWidgetBtn />
+      </div>
+    </div>
   )
 }
 
