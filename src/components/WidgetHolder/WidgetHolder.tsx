70cc9785cf61105bd225302b8537b02c28a54ac9
Yash Chaudhari <yashchaudhari008@gmail.com>
2024-01-24 19:32:01 +0530

    resolved build error

--- src/components/WidgetHolder/WidgetHolder.tsx
+++ src/components/WidgetHolder/WidgetHolder.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from "react";
+import { FormEvent, useState } from "react";
 import { addWidget, getWidgets, WidgetType } from "../../utils/widgetsUtils";
 import type { WidgetData } from "../../utils/widgetsUtils";
 import Modal from "../shared/Modal/Modal";
--- src/components/shared/InputField/InputField.tsx
+++ src/components/shared/InputField/InputField.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 type InputFieldProps = {
 	id: string;
 	type?: string;
