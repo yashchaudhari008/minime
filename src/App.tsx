639f9fc48f144b8b97f509a48383bad76fc23d43
linyers <linyers666@gmail.com>
2024-01-29 12:11:56 -0300

    change import order

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import TopBar from "./components/TopBar/TopBar";
 import WidgetHolder from "./components/WidgetHolder/WidgetHolder";
-import styles from "./app.module.scss";
 import Footer from "./components/Footer/Footer";
+import styles from "./app.module.scss";
 
 function App() {
 	return (
