8e6719cec785f9af4b82568c73a1753523bb8a67
linyers <134330318+linyers@users.noreply.github.com>
2024-01-31 05:21:42 -0300

    [FEATURE] : Footer component (#8)
    * add: Footer component
    * add: free brands font awesome icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import TopBar from "./components/TopBar/TopBar";
 import WidgetHolder from "./components/WidgetHolder/WidgetHolder";
+import Footer from "./components/Footer/Footer";
 import styles from "./app.module.scss";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 			<div className={styles.app}>
 				<TopBar />
 				<WidgetHolder />
+				<Footer />
 			</div>
 		</>
 	);
