f01c883ab5b3744cad2a2d5108b04e4664d79750
HappyLife2 <130459769+HappyLife2@users.noreply.github.com>
2024-01-31 16:17:07 +0530

    added rearrangement logic

--- src/components/WidgetHolder/WidgetHolder.tsx
+++ src/components/WidgetHolder/WidgetHolder.tsx
@@ -1,22 +1,55 @@
 import { FormEvent, useState } from "react";
-import { addWidget, getWidgets, WidgetType } from "../../utils/widgetsUtils";
-import type { WidgetData } from "../../utils/widgetsUtils";
+import { useDrag, useDrop, DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { addWidget, getWidgets, WidgetType, WidgetData } from "../../utils/widgetsUtils";
 import Modal from "../shared/Modal/Modal";
 import InputField from "../shared/InputField/InputField";
 import AddNewWidgetBtn from "../widgets/AddNewWidgetBtn/AddNewWidgetBtn";
 import BookmarkWidget from "../widgets/BookmarkWidget/BookmarkWidget";
 import styles from "./widgetHolder.module.scss";
 
+// Move getWidgetDOM before DraggableWidget
+const getWidgetDOM = (widgetData: WidgetData, index: number) => {
+	switch (widgetData.type) {
+		case WidgetType.BookmarkWidget:
+			return <BookmarkWidget key={index} {...widgetData} />;
+	}
+};
+
+const DraggableWidget = ({
+	widgetData,
+	index,
+	moveWidget,
+}: {
+	widgetData: WidgetData;
+	index: number;
+	moveWidget: (fromIndex: number, toIndex: number) => void;
+}) => {
+	const [, ref] = useDrag({
+		type: "WIDGET",
+		item: { id: index, index },
+	});
+
+	const [, drop] = useDrop({
+		accept: "WIDGET",
+		hover: (draggedItem: { index: number }) => {
+			if (draggedItem.index !== index) {
+				moveWidget(draggedItem.index, index);
+				draggedItem.index = index;
+			}
+		},
+	});
+
+	return (
+		<div ref={(node) => ref(drop(node))} className={styles.widget}>
+			{getWidgetDOM(widgetData, index)}
+		</div>
+	);
+};
+
 const WidgetHolder = () => {
 	const [showAddNewWidgetModal, setShowAddNewWidgetModal] = useState(false);
 	const [widgetsData, setWidgetsData] = useState(getWidgets());
-
-	const getWidgetDOM = (widgetData: WidgetData, index: number) => {
-		switch (widgetData.type) {
-			case WidgetType.BookmarkWidget:
-				return <BookmarkWidget key={index} {...widgetData} />;
-		}
-	};
 
 	const openAddNewWidgetModal = () => {
 		setShowAddNewWidgetModal(true);
@@ -49,36 +82,46 @@ const WidgetHolder = () => {
 		closeAddNewWidgetModal();
 	};
 
-	return (
-		<div className={styles.widgetHolderWrapper}>
-			<div className={styles.widgetHolder}>
-				{widgetsData.map((widgetData, index) => {
-					return (
-						<div key={index} className={styles.widget}>
-							{getWidgetDOM(widgetData, index)}
-						</div>
-					);
-				})}
-				<AddNewWidgetBtn onClick={openAddNewWidgetModal} />
-			</div>
+	const moveWidget = (fromIndex: number, toIndex: number) => {
+		const updatedWidgets = [...widgetsData];
+		const [movedWidget] = updatedWidgets.splice(fromIndex, 1);
+		updatedWidgets.splice(toIndex, 0, movedWidget);
+		setWidgetsData(updatedWidgets);
+	};
 
-			<Modal showModal={showAddNewWidgetModal} headerText="New Bookmark">
-				<form className={styles.modalContent} onSubmit={onAddNewWidgetModalSubmit}>
-					<div className={styles.formInputHolder}>
-						<InputField id="name" label="Name" />
-					</div>
-					<div className={styles.formInputHolder}>
-						<InputField id="url" label="Link" type="url" required />
-					</div>
-					<div className={styles.modalFooter}>
-						<button type="button" onClick={closeAddNewWidgetModal}>
-							Cancel
-						</button>
-						<button type="submit">Add</button>
-					</div>
-				</form>
-			</Modal>
-		</div>
+	return (
+		<DndProvider backend={HTML5Backend}>
+			<div className={styles.widgetHolderWrapper}>
+				<div className={styles.widgetHolder}>
+					{widgetsData.map((widgetData, index) => (
+						<DraggableWidget
+							key={index}
+							widgetData={widgetData}
+							index={index}
+							moveWidget={moveWidget}
+						/>
+					))}
+					<AddNewWidgetBtn onClick={openAddNewWidgetModal} />
+				</div>
+
+				<Modal showModal={showAddNewWidgetModal} headerText="New Bookmark">
+					<form className={styles.modalContent} onSubmit={onAddNewWidgetModalSubmit}>
+						<div className={styles.formInputHolder}>
+							<InputField id="name" label="Name" />
+						</div>
+						<div className={styles.formInputHolder}>
+							<InputField id="url" label="Link" type="url" required />
+						</div>
+						<div className={styles.modalFooter}>
+							<button type="button" onClick={closeAddNewWidgetModal}>
+								Cancel
+							</button>
+							<button type="submit">Add</button>
+						</div>
+					</form>
+				</Modal>
+			</div>
+		</DndProvider>
 	);
 };
 
