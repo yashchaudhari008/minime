e01907342ff13b384911a2f8c7992895a4511ca6
Yash Chaudhari <yashchaudhari008@gmail.com>
2024-04-07 17:08:59 +0530

    updated utils

--- src/utils/widgetsUtils.ts
+++ src/utils/widgetsUtils.ts
@@ -26,13 +26,31 @@ export const getWidgets = () => {
 };
 
 export const addWidget = (newWidgetData: WidgetData) => {
+	const oldWidgetsData = getWidgets();
 	try {
-		const oldWidgetsData = getWidgets();
 		const newWidgetsData = [...oldWidgetsData, newWidgetData];
 		localStorage.setItem("widgetsData", JSON.stringify(newWidgetsData));
+		return newWidgetsData;
 	} catch (e) {
 		console.error("Failed while adding new widget:", e);
 	}
+	return oldWidgetsData;
+};
+
+export const editWidget = (updatedWidgetData: WidgetData, atIndex: number) => {
+	const oldWidgetsData = getWidgets();
+	try {
+		// Filter widget to be editted
+		const newWidgetsData = oldWidgetsData.filter((_, ind) => atIndex !== ind);
+		// Insert the updated widget into newWidgetsData
+		newWidgetsData.splice(atIndex, 0, updatedWidgetData);
+		localStorage.setItem("widgetsData", JSON.stringify(newWidgetsData));
+
+		return newWidgetsData;
+	} catch (e) {
+		console.error("Failed while editing widget:", e);
+	}
+	return oldWidgetsData;
 };
 
 export const updateWidgetsData = (newWidgetsData: WidgetData[]) => {
