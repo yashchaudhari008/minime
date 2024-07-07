ba8db3170cecb95784bbc78db4cc8febd6193130
Yash Chaudhari <yashchaudhari008@gmail.com>
2024-07-07 19:43:17 +0530

    [FEATURE] Added support for extension  (#33)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
+import dotenv from "dotenv";
 import react from "@vitejs/plugin-react";
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/minime/",
+	base: `${process.env.BASE_URL || "/"}`,
 	plugins: [react()],
 });
