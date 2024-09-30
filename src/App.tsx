4a58ec21efa781a35960268dff8a3a218926bd0f
abishek1740P <abisheksivarajan@gmail.com>
2024-09-30 18:29:48 +0530

    feat : added scroll bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import TopBar from "./components/TopBar/TopBar";
 import WidgetHolder from "./components/WidgetHolder/WidgetHolder";
+// import ExampleComponent from "./components/ExampleComponent/ExampleComponent"; //This is an example of how the custom Scroll bar Works. 
 import Footer from "./components/Footer/Footer";
 import styles from "./app.module.scss";
 
@@ -9,6 +10,7 @@ function App() {
 			<div className={styles.app}>
 				<TopBar />
 				<WidgetHolder />
+				{/* <ExampleComponent /> */} 
 				<Footer />
 			</div>
 		</>
