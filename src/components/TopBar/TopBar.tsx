8d7a4628b5ffcae90a068a53edd9d23e96fb6638
Yash Chaudhari <yashchaudhari008@gmail.com>
2024-11-02 22:12:51 +0530

    Revert "[FEATURE] Settings page (#52)"
    This reverts commit 263f6b85a07088bc7fd0fb2b25011774e3cb5473.

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,24 +1,10 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGear } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from "react-router-dom";
 import Clock from "./Clock";
 import styles from "./topBar.module.scss";
 
 const TopBar = () => {
-	const navigate = useNavigate();
-
-	const openSettingsPage = () => {
-		navigate("/minime/settings");
-	};
 	return (
 		<div className={styles.topBar}>
 			<Clock />
-			<FontAwesomeIcon
-				icon={faGear}
-				className={styles.settingsIcon}
-				size="2xl"
-				onClick={openSettingsPage}
-			/>
 		</div>
 	);
 };
