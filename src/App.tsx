121127e07754bc028c64bec0730ab267b7678a78
Yash Chaudhari <yashchaudhari008@gmail.com>
2024-11-02 22:13:26 +0530

    Revert "[FEATURE] Settings page (#52)" (#55)
    This reverts commit 263f6b85a07088bc7fd0fb2b25011774e3cb5473.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Settings from "./pages/Settings/Settings";
+import TopBar from "./components/TopBar/TopBar";
+import WidgetHolder from "./components/WidgetHolder/WidgetHolder";
+import Footer from "./components/Footer/Footer";
+import styles from "./app.module.scss";
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/minime/settings" element={<Settings />} />
-			</Routes>
-		</BrowserRouter>
+		<>
+			<div className={styles.app}>
+				<TopBar />
+				<WidgetHolder />
+				<Footer />
+			</div>
+		</>
 	);
 }
 
