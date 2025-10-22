a60c833827f3b6b445d4fa247c95577b4fd685bb
Yash Chaudhari <yashchaudhari008@gmail.com>
2025-10-22 18:30:27 +0530

    onFocus caret move to end

--- src/components/NotepadWidgetModal/NotepadWidgetModal.tsx
+++ src/components/NotepadWidgetModal/NotepadWidgetModal.tsx
@@ -32,10 +32,16 @@ const NotepadWidgetModal = ({
 	const onChangeHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
 		setNotepadContent(e.target.value);
 	}
+	const onFocusHandler = (e: React.FocusEvent<HTMLTextAreaElement>) => {
+		// https://gist.github.com/piyonishi/409ecbd07f7b86b7da205ad61210a275
+		const tempContent = e.target.value;
+		e.target.value = "";
+		e.target.value = tempContent;
+	}
 
 	return (
 		<Modal showModal={show} onCloseClick={onClose} headerText="Quick Note" rightSideModal>
-			<textarea id="notes" autoFocus value={notepadContent} onChange={onChangeHandler} className={styles.notes} placeholder="Add a note here" />
+			<textarea id="notes" autoFocus onFocus={onFocusHandler} value={notepadContent} onChange={onChangeHandler} className={styles.notes} placeholder="Add a note here" />
 		</Modal>
 	);
 };
