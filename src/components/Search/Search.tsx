796c0384d3c94f038c2c7ec3206dd2a1a126d936
eldanielhumberto <gamekik92@gmail.com>
2025-11-14 22:42:08 -0600

    feat: the search input now works, and react-hook-form has also been added for better control

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,8 +1,28 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
 import styles from './search.module.scss';
 
+interface Inputs {
+  searchValue: string;
+}
+
 const Search = () => {
+  const { register, handleSubmit, reset } = useForm<Inputs>();
+
+  const onSearch: SubmitHandler<Inputs> = ({ searchValue }) => {
+    let url = "https://www.google.com/search?q=";
+    window.open(url + searchValue);
+
+    reset()
+  }
+
   return (
-    <form className={styles.searchForm}><input type="search" placeholder="Search..." /></form>
+    <form onSubmit={handleSubmit(onSearch)} className={styles.searchForm}>
+      <input
+        type="search"
+        placeholder="Search..."
+        {...register('searchValue')}
+      />
+    </form>
   )
 }
 
